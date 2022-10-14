13f39f6c0008443c43c6879f1bedbe545da5b8cf
Roman <onmyway2tokyo@gmail.com>
2022-10-15 01:09:42 +0300

    end

---
+++ src/components/ticketCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <ul class="ticket-list">
+    <li class="ticket-element" v-for="ticket of tickets" :key="ticket.id">
+      <div class="ticket__image-container">
+        <div :class="ticket.labelColor" class="ticket__label">{{ ticket.label }}</div>
+        <img class="ticket__image" :src="ticket.image" :alt="ticket.title">
+      </div>
+      <div class="ticket__content-container">
+        <div class="ticket__time-container">
+          <img class="ticket__time-image" :src="ticket.timeImage" :alt="ticket.title">
+          <div class="ticket__time">{{ ticket.time }}</div>
+        </div>
+        <div class="ticket__title">{{ ticket.title }}</div>
+        <div>
+          <div class="ticket__details-list">
+            <div class="ticket__details-element" v-for="detail in ticket.details" :key="detail.name">
+              <img class="ticket__details-image" :src="ticket.checkImage" :alt="ticket.title">
+              {{ detail }}
+            </div>
+          </div>
+          <div class="ticket__flex-container">
+            <div class="ticket__today">Ближайший рейс сегодня</div>
+            <div class="ticket__times-container">
+              <ul class="ticket__times-list">
+                <li class="ticket__times-element" v-for="openTime in ticket.openTimes.slice(0,2)" :key="openTime.id">
+                  {{ openTime.time }}
+                </li>
+              </ul>
+              <ul v-show="this.showTimes"  class="ticket__times-list">
+                <li class="ticket__times-element" v-for="openTime in ticket.openTimes.slice(2,100)" :key="openTime.id">
+                  {{ openTime.time }}
+                </li>
+              </ul>
+            </div>
+            <div @click.prevent="remakeClass()" class="ticket__else-button">eще</div>
+          </div>
+        </div>
+        <div class="ticket__price-and-button">
+          <div class="ticket__price-container">
+            <div class="ticket__price">{{ ticket.price }}<span>&#x20bd;</span></div>
+            <div class="ticket__discount">{{ ticket.discount }}</div>
+          </div>
+          <button class="ticket__button">Подробнее</button>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+
+import tickets from "@/data/tickets";
+
+export default {
+  data() {
+    return {
+      tickets,
+      showTimes: false,
+    }
+  },
+  methods: {
+    remakeClass() {
+      this.showTimes = !this.showTimes;
+    },
+  },
+  computed: {}
+}
+</script>
+
+
---
+++ src/data/tickets.js
@@ -0,0 +1,119 @@
+export default [
+    {
+        id: 1,
+        time: '2 часа',
+        title: 'Обзорная экскурсия по рекам и каналам  с остановками Hop on Hop Off 2019',
+        price: 900,
+        discount: '1200 р на причале',
+        details:[
+            'Билет на целый день',
+            'Неограниченное число катаний',
+            '6 остановок у главных достопримечательностей',
+            ],
+        openTimes:[
+            {
+                id: 1,
+                time:'11:00',
+            },
+            {
+                id: 2,
+                time:'12:00',
+            },
+            {
+                id: 3,
+                time:'13:00',
+            },
+            {
+                id: 4,
+                time:'14:00',
+            },
+            {
+                id: 5,
+                time:'15:00',
+            },
+            {
+                id: 6,
+                time:'16:00',
+            },
+            {
+                id: 7,
+                time:'17:00',
+            },
+            {
+                id: 8,
+                time:'18:00',
+            },
+        ],
+        label: 'Новинка',
+        labelColor: 'blue',
+        image: require('../assets/images/bridge.jpg'),
+        timeImage: require('../assets/images/clock.png'),
+        checkImage: require('../assets/images/check.png'),
+    },
+    {
+        id: 2,
+        time: '2 часа',
+        title: 'Обзорная экскурсия по рекам и каналам  с остановками Hop on Hop Off 2020',
+        price: 900,
+        discount: '',
+        details:[
+            'Билет на целый день',
+            'Неограниченное число катаний',
+            '6 остановок у главных достопримечательностей',
+        ],
+        openTimes:[
+            {
+                id: 1,
+                time:'16:00',
+            },
+            {
+                id: 2,
+                time:'17:00',
+            },
+            {
+                id: 3,
+                time:'17:00',
+            },
+            {
+                id: 4,
+                time:'17:00',
+            },
+        ],
+        label: 'Круглый год',
+        labelColor: 'yellow',
+        image: require('../assets/images/bridge.jpg'),
+        timeImage: require('../assets/images/clock.png'),
+        checkImage: require('../assets/images/check.png'),
+    },
+    {
+        id: 3,
+        time: '2 часа',
+        title: 'Обзорная экскурсия по рекам и каналам  с остановками Hop on Hop Off 2021',
+        price: 900,
+        discount: '1200 р на причале',
+        details:[
+            'Билет на целый день',
+            'Неограниченное число катаний',
+            '6 остановок у главных достопримечательностей',
+        ],
+        openTimes:[
+            {
+                id: 1,
+                time:'18:00',
+            },
+            {
+                id: 2,
+                time:'19:00',
+            },
+            {
+                id: 3,
+                time:'20:00',
+            },
+        ],
+        label: 'Новинка',
+        labelColor: 'yellow',
+        image: require('../assets/images/bridge.jpg'),
+        timeImage: require('../assets/images/clock.png'),
+        checkImage: require('../assets/images/check.png'),
+    },
+];
